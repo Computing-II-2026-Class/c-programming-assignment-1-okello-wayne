0d91de7b3fa531a542638210ac9c3130c494c22f
okello-wayne <wayneredemption@outlook.com>
2026-03-22 04:18:08 +0300

    Add electricity billing calculation functionality

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,21 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Test User */
+/* Student Number: 00/TEST/001 */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float units, cost_per_unit, total_bill;
+
+    printf("Enter units consumed: ");
+    scanf("%f", &units);
+
+    printf("Enter cost per unit: ");
+    scanf("%f", &cost_per_unit);
+
+    total_bill = units * cost_per_unit;
+
+    printf("Total bill: %.2f UGX\n", total_bill);
 
     return 0;
 }
